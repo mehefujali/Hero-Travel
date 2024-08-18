8ca5702ae9e93f46c1545d1bb746a9cba9ba7dff
Mehefuj Ali <mehefujali30@gmail.com>
2024-08-18 13:14:07 +0530

    nave and hero section with responsive

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'hero-background': "url(./Images/Group 13 (1) (1).png)"
+            }
+        },
+    },
+    plugins: [],
+}
